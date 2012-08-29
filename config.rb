2c0eecb87263142a81678bca6c0943a76d4c3f6a
airatshigapov <airus.kzn@gmail.com>
2012-08-29 15:40:59 +0400

    started localization

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-### 
+###
 # Compass
 ###
 
@@ -16,13 +16,13 @@
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -47,6 +47,8 @@
 #   end
 # end
 
+activate :i18n
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -57,21 +59,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
